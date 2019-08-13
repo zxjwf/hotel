5738e7613cff9f2ffb17a58203f06ab90b4ad417
zxj <44485524@qq.com>
2019-08-13 15:51:06 +0800

    2019.8.13 allbackup

---
+++ src/main/java/com/myhotel/common/vo/Node.java
@@ -0,0 +1,34 @@
+package com.myhotel.common.vo;
+
+import java.io.Serializable;
+
+public class Node implements Serializable {
+    private static final long seriaVersionUID=28789274274923979L;
+    private Integer id;
+    private String name;
+    private Integer parentId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+}
